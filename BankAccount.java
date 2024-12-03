e7f4fcf1cdd4277a59b19ec92529eeaac7252a24
Akhan194-ai <abdullahfirasat194@gmail.com>
2024-12-03 15:22:29 +0500

    lab07SCDCLASS1

--- BankAccount.java
+++ BankAccount.java
@@ -1,45 +1,32 @@
-package lab06SCD;
+package lab05;
 
 public class BankAccount {
-	private double balance;
+	   private double balance;
 
 	    public BankAccount(double initialBalance) {
 	        this.balance = initialBalance;
 	    }
 
-	    // Synchronized method to withdraw money from the account
-	    public synchronized boolean withdraw(double amount) {
-	        if (amount <= balance) {
-	            System.out.println(Thread.currentThread().getName() + " is withdrawing: " + amount);
-	            balance -= amount;
-	            System.out.println(Thread.currentThread().getName() + " completed the withdrawal. New balance: " + balance);
-	            return true;
+	    // Synchronized method to ensure only one thread can withdraw at a time
+	    public synchronized void withdraw(double amount) {
+	        if (amount > balance) {
+	            System.out.println(Thread.currentThread().getName() + ": Insufficient funds for withdrawal of " + amount);
 	        } else {
-	            System.out.println(Thread.currentThread().getName() + " attempted to withdraw " + amount + " but insufficient funds. Current balance: " + balance);
-	            return false;
+	            System.out.println(Thread.currentThread().getName() + ": Withdrawing " + amount);
+	            balance -= amount;
+	            System.out.println(Thread.currentThread().getName() + ": New Balance: " + balance);
 	        }
+	    }
+
+	    // Synchronized method to ensure only one thread can deposit at a time
+	    public synchronized void deposit(double amount) {
+	        System.out.println(Thread.currentThread().getName() + ": Depositing " + amount);
+	        balance += amount;
+	        System.out.println(Thread.currentThread().getName() + ": New Balance: " + balance);
 	    }
 
 	    public double getBalance() {
 	        return balance;
-	    }
-	}
-
-	class UserThread extends Thread {
-	    private BankAccount account;
-	    private double amount;
-
-	    public UserThread(BankAccount account, double amount, String name) {
-	        this.account = account;
-	        this.amount = amount;
-	        this.setName(name);  // Set the name of the thread (user)
-	    }
-
-	    @Override
-	    public void run() {
-	        if (!account.withdraw(amount)) {
-	            System.out.println(getName() + " could not withdraw " + amount);
-	        }
 	    }
 	}
 
