f7f0b8b8895dbf71998a469c69d3f0bdd569d8f6
Akhan194-ai <abdullahfirasat194@gmail.com>
2025-01-07 21:31:37 +0500

    LAB11SCDTASK2

---
+++ BillPaymentService.java
@@ -0,0 +1,9 @@
+package lab11scd;
+
+class BillPaymentService implements BankService {
+    @Override
+    public void performService() {
+        System.out.println("Processing bill payment...");
+        // Logic for processing bill payments
+    }
+}
