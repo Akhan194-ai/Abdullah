c846fdf96b42212c8d947541c4ab42f068c4d952
Akhan194-ai <abdullahfirasat194@gmail.com>
2025-01-07 21:33:02 +0500

    LAB11SCDTASK3

---
+++ AccountOpeningService.java
@@ -0,0 +1,9 @@
+package lab11scd;
+
+class AccountOpeningService implements BankService {
+    @Override
+    public void performService() {
+        System.out.println("Opening a new account...");
+        // Logic for opening new accounts
+    }
+}
