c0aaa581b6d0662afe797497686c1e0a4e81a5a8
Akhan194-ai <abdullahfirasat194@gmail.com>
2025-01-07 21:36:51 +0500

    LAB11SCDTASK5

---
+++ LoanFollowUpService.java
@@ -0,0 +1,9 @@
+package lab11scd;
+
+class LoanFollowUpService implements BankService {
+    @Override
+    public void performService() {
+        System.out.println("Contacting loan defaulters...");
+        // Logic for following up with loan defaulters
+    }
+}
